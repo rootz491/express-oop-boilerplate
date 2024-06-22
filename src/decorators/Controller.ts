37f9b97a488e9519f1c354061d62710e1b46ab07
rootz491 <karansh491@gmail.com>
2024-06-23 03:16:56 +0530

    chore: clean up decorators

--- src/decorators/Controller.ts
+++ src/decorators/Controller.ts
@@ -3,30 +3,5 @@ import "reflect-metadata";
 export function Controller(prefix: string): ClassDecorator {
 	return (target: any) => {
 		Reflect.defineMetadata("prefix", prefix, target);
-
-		/*
-		// Store a reference to the original constructor
-		const original = target;
-
-		// A utility function to generate instances of a class
-		function construct(constructor: any, args: any) {
-			const c: any = function () {
-				return new constructor(...args);
-			};
-			c.prototype = constructor.prototype;
-			return new c();
-		}
-
-		// The new constructor behavior
-		const f: any = function (...args: any) {
-			return construct(original, args);
-		};
-
-		// Copy prototype so instanceof operator still works
-		f.prototype = original.prototype;
-
-		// Return the new constructor (will override original)
-		return f;
-		*/
 	};
 }
